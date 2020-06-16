ee610e12789904175ee3c97479676db9c26a9266
wenqiuren <wenqiuren@users.noreply.github.com>
2020-06-16 21:04:48 +0800

    QW

--- atguigu_parent/guli_teacher/src/main/java/com/guli/teacher/TeacherApplication.java
+++ atguigu_parent/guli_teacher/src/main/java/com/guli/teacher/TeacherApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.guli.teacher.mapper")
 public class TeacherApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeacherApplication.class,args);
--- atguigu_parent/guli_teacher/src/main/java/com/guli/teacher/mapper/TeacherMapper.java
+++
@@ -1,9 +0,0 @@
-package com.guli.teacher.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.guli.teacher.entity.Teacher;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface TeacherMapper extends BaseMapper<Teacher> {
-}
